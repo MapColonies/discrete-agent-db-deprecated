f45c43c1c5daf4f302c9202ab77369336aca2c5f
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-27 13:11:50 +0200

    test(api): integration ttest base

--- tests/integration/status/status.spec.ts
+++ tests/integration/status/status.spec.ts
@@ -1,11 +1,10 @@
 import httpStatusCodes from 'http-status-codes';
 import { container } from 'tsyringe';
-import { IResourceNameModel } from '../../../src/resourceName/models/resourceNameManager';
-
+import { IStatus } from '../../../src/status/interfaces';
 import { registerTestValues } from '../testContainerConfig';
 import * as requestSender from './helpers/requestSender';
 
-describe('resourceName', function () {
+describe('Status', function () {
   beforeAll(function () {
     registerTestValues();
     requestSender.init();
@@ -15,15 +14,13 @@ describe('resourceName', function () {
   });
 
   describe('Happy Path', function () {
-    it('should return 200 status code and the resource', async function () {
+    it('should return 200 status code and the status', async function () {
       const response = await requestSender.getResource();
 
       expect(response.status).toBe(httpStatusCodes.OK);
 
-      const resource = response.body as IResourceNameModel;
-      expect(resource.id).toEqual(1);
-      expect(resource.name).toEqual('ronin');
-      expect(resource.description).toEqual('can you do a logistics run?');
+      const status = response.body as IStatus;
+      expect(status.isWatching).toEqual(true);
     });
   });
   describe('Bad Path', function () {
