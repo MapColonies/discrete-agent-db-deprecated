c14760460dd70fe1a3b69c436768677727e30c55
shimoncohen <33935191+shimoncohen@users.noreply.github.com>
2021-04-20 15:35:21 +0300

    chore(configurations): add ssl options (#27)

--- src/DAL/connectionManager.ts
+++ src/DAL/connectionManager.ts
@@ -1,8 +1,9 @@
+import { readFileSync } from 'fs';
 import { createConnection, Connection, ObjectType } from 'typeorm';
 import { inject, singleton } from 'tsyringe';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { ConnectionOptions } from 'typeorm';
 import { Services } from '../common/constants';
-import { IConfig, ILogger } from '../common/interfaces';
+import { IConfig, ILogger, IDbConfig } from '../common/interfaces';
 import { HTTP_INTERNAL_SERVER_ERROR } from '../common/httpErrors';
 import { SettingsRepository } from './repositories/settingsRepository';
 import { LayerHistoryRepository } from './repositories/layerHistoryRepository';
@@ -14,15 +15,25 @@ export class ConnectionManager {
   public constructor(@inject(Services.LOGGER) private readonly logger: ILogger, @inject(Services.CONFIG) private readonly config: IConfig) {}
 
   public async init(): Promise<void> {
-    const connectionConfig = this.config.get<PostgresConnectionOptions>('typeOrm');
+    const connectionConfig = this.config.get<IDbConfig>('typeOrm');
     this.logger.log('info', `connection to database ${connectionConfig.database as string} on ${connectionConfig.host as string}`);
     try {
-      this.connection = await createConnection(connectionConfig);
+      const options = this.createConnectionOptions(connectionConfig);
+      this.connection = await createConnection(options);
     } catch (err) {
       const errString = JSON.stringify(err);
       this.logger.log('error', `failed to connect to database: ${errString}`);
       throw HTTP_INTERNAL_SERVER_ERROR;
     }
+  }
+
+  private createConnectionOptions(dbConfig: IDbConfig): ConnectionOptions {
+    const { enableSslAuth, sslPaths, ...connectionOptions } = dbConfig;
+    if (enableSslAuth) {
+      connectionOptions.password = undefined;
+      connectionOptions.ssl = { key: readFileSync(sslPaths.key), cert: readFileSync(sslPaths.cert), ca: readFileSync(sslPaths.ca) };
+    }
+    return connectionOptions;
   }
 
   public isConnected(): boolean {
