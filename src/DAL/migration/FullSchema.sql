2a3e761c203febf1fc43600849b4577f0c87db7e
Asaf Masa <ASAFMAS@rafael.co.il>
2021-08-16 10:46:36 +0300

    feat: fix cr comments

--- src/DAL/migration/FullSchema.sql
+++ src/DAL/migration/FullSchema.sql
@@ -12,12 +12,12 @@ CREATE TABLE public."layer_history"
   "status" "operation_status_enum" NOT NULL DEFAULT 'inProgress'::"operation_status_enum",
   "createdOn" timestamp with time zone NOT NULL DEFAULT now(),
   "updatedOn" timestamp with time zone NOT NULL DEFAULT now(),
-  CONSTRAINT "PK_727c95a6a04ec30834a681ce004" PRIMARY KEY (directory)
+  CONSTRAINT "layer_history_pkey" PRIMARY KEY (directory)
 );
 
 CREATE TABLE public."setting"
 (
   "key" character varying(300) NOT NULL,
   "value" character varying(300) NOT NULL,
-  CONSTRAINT "PK_1c4c95d773004250c157a744d6e" PRIMARY KEY (key)
+  CONSTRAINT "setting_pkey" PRIMARY KEY (key)
 );
